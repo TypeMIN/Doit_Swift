a1d1229956a8648c5bd9c92752b11ec4964adb9e
TypeMIN <typemin@naver.com>
2021-11-11 18:08:48 +0900

    21-11-11 study

--- Challenge08/Challenge08/ViewController.swift
+++ Challenge08/Challenge08/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Map
+//  Challenge08
 //
 //  Created by 이민식 on 2021/11/11.
 //
@@ -12,7 +12,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
   @IBOutlet var myMap: MKMapView!
   @IBOutlet var lblLocationInfo1: UILabel!
-  @IBOutlet var lblLocationInfo2: UILabel!
+  @IBOutlet var lblLocatinoInfo2: UILabel!
   
   let locationManager = CLLocationManager()
   
@@ -21,14 +21,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     // Do any additional setup after loading the view.
     
     lblLocationInfo1.text = ""
-    lblLocationInfo2.text = ""
+    lblLocatinoInfo2.text = ""
     locationManager.delegate = self
     locationManager.desiredAccuracy = kCLLocationAccuracyBest
     locationManager.requestWhenInUseAuthorization()
     locationManager.startUpdatingLocation()
     myMap.showsUserLocation = true
   }
-
+  
   func goLocation(latitudeValue: CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D {
     let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
     let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
@@ -37,14 +37,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     return pLocation
   }
   
-  func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double, titile strTitle:String, subtitle strSubtitle:String) {
-    let annotation = MKPointAnnotation()
-    annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
-    annotation.title = strTitle
-    annotation.subtitle = strSubtitle
-    myMap.addAnnotation(annotation)
+  func setAnnotation(latitudeValue: CLLocationDegrees, longituteValue : CLLocationDegrees, delta span :Double, title strTitle:String, subtitle strSubtitle:String) {
+    let annotaion = MKPointAnnotation()
+    annotaion.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longituteValue, delta: span)
+    annotaion.title = strTitle
+    annotaion.subtitle = strSubtitle
+    myMap.addAnnotation(annotaion)
   }
-
+  
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     let pLocation = locations.last
     _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
@@ -63,28 +63,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
       }
       
       self.lblLocationInfo1.text = "현재 위치"
-      self.lblLocationInfo2.text = address
-      
+      self.lblLocatinoInfo2.text = address
     })
+    
+    locationManager.stopUpdatingLocation()
   }
-  
+
   @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     if sender.selectedSegmentIndex == 0 {
       self.lblLocationInfo1.text = ""
-      self.lblLocationInfo2.text = ""
+      self.lblLocatinoInfo2.text = ""
       locationManager.startUpdatingLocation()
     } else if sender.selectedSegmentIndex == 1 {
-      setAnnotation(latitudeValue: 37.751853, longitudeValue: 128.87605740000004, delta: 1, titile: "한국폴리텍대학 강릉캠퍼스", subtitle: "강원도 강릉시 남산초교길 121")
+      setAnnotation(latitudeValue: 35.57340, longituteValue: 129.18965, delta: 0.1, title: "UNIST", subtitle: "울산광역시 울주군 언양읍 유니스트길 50")
       self.lblLocationInfo1.text = "보고 계신 위치"
-      self.lblLocationInfo2.text = "한국폴리텍대학 강릉캠퍼스"
+      self.lblLocatinoInfo2.text = "UNIST"
     } else if sender.selectedSegmentIndex == 2 {
-      setAnnotation(latitudeValue: 37.556876, longitudeValue: 126.914066, delta: 0.1, titile: "이지스퍼블리싱", subtitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
+      setAnnotation(latitudeValue: 37.92141, longituteValue: 128.09079, delta: 0.01, title: "KCTC", subtitle: "강원도 인제군 남면 응봉길")
       self.lblLocationInfo1.text = "보고 계신 위치"
-      self.lblLocationInfo2.text = "이지스퍼블리싱 출판사"
+      self.lblLocatinoInfo2.text = "KCTC"
+    } else if sender.selectedSegmentIndex == 3 {
+      setAnnotation(latitudeValue: 36.89082, longituteValue: 126.71750, delta: 0.01, title: "우리집", subtitle: "충청남도 당진시 송악읍 광명길 71")
+      self.lblLocationInfo1.text = "보고 계신 위치"
+      self.lblLocatinoInfo2.text = "우리집"
     }
   }
   
 }
-
-
 
